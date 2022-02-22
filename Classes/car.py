adbf2101319756c3004d1a441ae0344b7cf33ab4
tony-ruppert <99216249+tony-ruppert@users.noreply.github.com>
2022-02-22 09:32:36 -0600

    update

--- Classes/car.py
+++ Classes/car.py
@@ -3,10 +3,46 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
 
     def get_discriptive_name(self):
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name.title()
+    
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it.")
 
-my_new_car = Car('audi', 'a4', 2019)
-print(my_new_car.get_discriptive_name())
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll back the odometer.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        # Add the given amount to the odometer reading.
+        self.odometer_reading += miles
+
+class Battery:
+    def __init__(self, battery_size=75):
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        print(f"This car has a {self.battery_size}-kWh battery.")
+    
+    def get_range(self):
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+
+        print(f"This car can go about {range} miles on a full charge.")
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
