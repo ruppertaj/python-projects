2d72d2953cace33ef784d48f6000b2c0cb538d4e
tony-ruppert <99216249+tony-ruppert@users.noreply.github.com>
2022-02-22 09:46:22 -0600

    updates

--- Classes/my_car.py
+++ Classes/my_car.py
@@ -1,4 +1,4 @@
-from car.py import Car
+from car import Car
 
 my_new_car = Car('audi', 'a4', 2019)
 print(my_new_car.get_descriptive_name())
---
+++ Classes/my_cars.py
@@ -0,0 +1,7 @@
+import car
+
+my_beetle = car.Car('volkswagon', 'beetle', 1967)
+print(my_beetle.get_descriptive_name())
+
+my_tesla = car.ElectricCar('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
