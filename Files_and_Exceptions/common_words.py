a6f6e953a7ff6ad1392e2e2ea6f4538acd056f69
tony-ruppert <99216249+tony-ruppert@users.noreply.github.com>
2022-02-22 15:28:46 -0600

    new

---
+++ Files_and_Exceptions/common_words.py
@@ -0,0 +1,3 @@
+line = "Row, row, row your boat"
+line.count('row')
+line.lower().count('row')
--- Files_and_Exceptions/write_message.py
+++ Files_and_Exceptions/write_message.py
@@ -1,5 +1,6 @@
 filename = '/Users/tony.ruppert/Documents/GitHub/python-projects/Files_and_Exceptions/programming.txt'
 
-with open(filename, 'w') as file_object:
-    file_object.write("I love programming.")
+with open(filename, 'a') as file_object:
+    file_object.write("I also love finding meaning in large datasets.\n")
+    file_object.write("I love creating apps that can run in a browser.\n")
 
