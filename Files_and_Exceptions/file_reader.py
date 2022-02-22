debdc668d170fbf714f6b175a31ef952e74fdc69
tony-ruppert <99216249+tony-ruppert@users.noreply.github.com>
2022-02-22 10:46:54 -0600

    all

--- Files_and_Exceptions/file_reader.py
+++ Files_and_Exceptions/file_reader.py
@@ -1,3 +1,3 @@
-with open('pi_digits.txt') as file_object:
+with open('/Users/tony.ruppert/Documents/GitHub/python-projects/Files_and_Exceptions/pi-digits.txt') as file_object:
     contents = file_object.read()
-print(contents)
+print(contents.rstrip())
---
+++ Files_and_Exceptions/write_message.py
@@ -0,0 +1,5 @@
+filename = '/Users/tony.ruppert/Documents/GitHub/python-projects/Files_and_Exceptions/programming.txt'
+
+with open(filename, 'w') as file_object:
+    file_object.write("I love programming.")
+
