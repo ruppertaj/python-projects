b02075853b123dbceda67511f445467efaf6355c
tony-ruppert <99216249+tony-ruppert@users.noreply.github.com>
2022-02-23 09:45:41 -0600

    new

---
+++ Part1-Basics/Testing/survey.py
@@ -0,0 +1,22 @@
+class AnonymousSurvey:
+    #Collect anonymous answers to a survey question.
+
+    def __init__(self, question):
+        #Store a question, and prepare to store responses
+        self.question = question
+        self.responses = []
+
+    def show_question(self):
+        #show the survey question
+        print(self.question)
+
+    def store_response(self, new_response):
+        #store a singe response to the survey
+        self.responses.append(new_response)
+
+    def show_results(self):
+        #show all the responses that have been given
+        print("Survey says:")
+        for response in self.responses:
+            print(f"- {response}")
+            
